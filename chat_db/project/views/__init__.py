20b4b46c344345016937a84e59e2e332a9a781f1
kabir <kabir.belloit@gmail.com>
2026-05-15 11:39:42 -0500

    fix: add messages_bp Blueprint to views __init__
    - messages_bp was missing from views/__init__.py
    - ImportError on chat_db startup
    - Blueprint now properly exported

--- chat_db/project/views/__init__.py
+++ chat_db/project/views/__init__.py
@@ -1,2 +1,7 @@
 # coding=utf-8
 from __future__ import unicode_literals, print_function, absolute_import, division
+from flask import Blueprint
+
+messages_bp = Blueprint('messages', __name__)
+
+from project.views import views  # noqa: E402, F401
