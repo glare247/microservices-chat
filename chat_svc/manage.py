4fa91995e774d62ec53184d53972aacb9622f9a3
kabir <kabir.belloit@gmail.com>
2026-06-08 21:07:46 -0500

    fix: add eventlet monkey_patch to manage.py
    FILE: chat_svc/manage.py
    
    PROBLEM:
      eventlet DNS resolver broken
      chat-db DNS lookup times out
      WebSocket connection drops
    
    FIX:
      Add eventlet.monkey_patch() first
      before any other imports
      This patches all stdlib modules
      including socket and DNS
      Fixes DNS resolution in GKE

--- chat_svc/manage.py
+++ chat_svc/manage.py
@@ -1,8 +1,15 @@
-# encoding: utf-8
+import eventlet
+eventlet.monkey_patch()
 
 from project.main import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port="8080", debug=True, log_output=True)  # nosec B104
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port="8080",
+        debug=True,
+        log_output=True
+    )  # nosec B104
